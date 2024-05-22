b54bebe94494575b04527c120ff745a8c18a217e
ayush2609123 <118757107+ayush2609123@users.noreply.github.com>
2024-05-22 14:01:30 +0000

    done

---
+++ 01_BasicsJS/01_Variables.js
@@ -0,0 +1,9 @@
+const accountID=144553 // cannot be chnaged
+let email="ayush@google"
+var pass="123"
+city="Delhi"
+console.table([accountID,email,pass,city])
+/* 
+we dont use var 
+because of block scope and functional scope
+*/
